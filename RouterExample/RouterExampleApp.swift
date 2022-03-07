b6cb2c192e72066ca6904727884455cf13b421a2
Carl Funk <carlalanfunk@outlook.com>
2022-03-07 10:19:36 -0600

    Initial Commit

---
+++ RouterExample/RouterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterExampleApp.swift
+//  RouterExample
+//
+//  Created by Carl Funk on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RouterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
